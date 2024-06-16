5cdba6382fa95a7e3d16f5497b33e46df127f9ad
SirArthur19 <artant0315@gmail.com>
2024-06-15 21:58:39 -0500

    Clase 15_06_2024 parte final

--- app/src/main/java/com/cibertec/cibertecapp/notas/NotaActivity.kt
+++ app/src/main/java/com/cibertec/cibertecapp/notas/NotaActivity.kt
@@ -1,4 +1,79 @@
 package com.cibertec.cibertecapp.notas
 
-class NotaActivity {
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cibertec.cibertecapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class NotaActivity:AppCompatActivity() {
+
+    private lateinit var viewModel: NotaViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nota)
+
+        viewModel = ViewModelProvider(this)[NotaViewModel::class.java]
+
+        val recyclerNotas = findViewById<RecyclerView>(R.id.recyclerNotas)
+        val floatingRegister = findViewById<FloatingActionButton>(R.id.floatingRegister)
+        floatingRegister.setOnClickListener{
+            registerAndUpdateNote()
+        }
+
+        val adapter = NotaAdapter()
+        recyclerNotas.adapter = adapter
+        recyclerNotas.layoutManager = LinearLayoutManager(this)
+        viewModel.getNotas().observe(this){ notas ->
+            if (notas.isNotEmpty()){
+                notas?.let {
+                    adapter.setNotas(notas)
+                }
+            }
+        }
+
+    }
+
+    private fun registerAndUpdateNote() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_nota, null)
+
+        val titleAlertNote = "Registrar nota"
+
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle(titleAlertNote)
+
+        val mAlertDialog = mBuilder.show()
+
+        val buttonCreate  = mDialogView.findViewById<Button>(R.id.btnCreate)
+        val editTextTitleCreate  = mDialogView.findViewById<EditText> (R.id.edtTitleNote)
+        val editTextDescriptionCreate  = mDialogView.findViewById<EditText>(R.id.edtDescriptionNote)
+
+        buttonCreate.setOnClickListener {
+
+            mAlertDialog.dismiss()
+
+            val titleNote = editTextTitleCreate.text.toString()
+            val descriptionNote = editTextDescriptionCreate.text.toString()
+            val currentDateTime = LocalDateTime.now().formatChangeNote()
+
+            val nota = Nota(currentDateTime, titleNote,
+                descriptionNote)
+            viewModel.insertNota(nota)
+
+        }
+
+    }
+
+    fun LocalDateTime.formatChangeNote() : String
+            = this.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
+
 }
